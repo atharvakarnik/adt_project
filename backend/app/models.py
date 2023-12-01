627f5ea2459bc774f24f330289050a91506fe08b
Atharva Karnik <viroo.28@gmail.com>
2023-12-01 12:03:10 -0500

    Added login logic

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,4 +5,16 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True)
-    # Define other fields like password_hash, is_manager, etc.
+    password_hash = db.Column(db.String(128))
+    is_manager = db.Column(db.Boolean, default=False)
+
+    # Relationship with Timesheet
+    timesheets = db.relationship('Timesheet', backref='user', lazy=True)
+
+class Timesheet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    clock_in_time = db.Column(db.DateTime, nullable=False)
+    clock_out_time = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    # Add other fields as necessary
