b482866cec8175847eaf32099bf350d270042e6c
Atharva Karnik <viroo.28@gmail.com>
2023-12-01 12:59:18 -0500

    Modified adding employee functionality

--- backend/app/add_user.py
+++ backend/app/add_user.py
@@ -1,20 +1,42 @@
 from app import create_app, db
-from app.models import User
-from werkzeug.security import generate_password_hash
+from app.models import Employee, Department, Location, LoginCredentials
 
 app = create_app()
 with app.app_context():
-    username = "new_user"  # Replace with the desired username
-    password = "password123"  # Replace with the desired password
-    is_manager = False  # Set to True if this user is a manager
+    # Assume these are the values collected from the form
+    emp_name = "Jane Doe"
+    dept_name = "Development"
+    location_name = "Headquarters"
+    username = "janedoe"
+    password = "strong_password123"
+    is_manager = True
 
-    # Check if user already exists
-    existing_user = User.query.filter_by(username=username).first()
-    if existing_user:
-        print("User already exists!")
-    else:
-        # Create new user
-        new_user = User(username=username, password_hash=generate_password_hash(password), is_manager=is_manager)
-        db.session.add(new_user)
+    # Add Department if it doesn't exist
+    department = Department.query.filter_by(dept_name=dept_name).first()
+    if not department:
+        department = Department(dept_name=dept_name)
+        db.session.add(department)
+
+    # Add Location if it doesn't exist
+    location = Location.query.filter_by(location_name=location_name).first()
+    if not location:
+        location = Location(location_name=location_name)
+        db.session.add(location)
+
+    # Commit new department and location to obtain IDs
+    db.session.commit()
+
+    # Check if username already exists in LoginCredentials
+    if not LoginCredentials.query.filter_by(username=username).first():
+        # Add new Employee
+        new_employee = Employee(emp_name=emp_name, department=department, location=location)
+        db.session.add(new_employee)
+        db.session.flush()  # This will populate new_employee.emp_id
+
+        # Add LoginCredentials for the new employee
+        new_login_credential = LoginCredentials(username=username, password_hash=generate_password_hash(password), is_manager=is_manager, employee=new_employee)
+        db.session.add(new_login_credential)
         db.session.commit()
-        print(f"User {username} added successfully.")
+        print(f"New employee {emp_name} with username {username} added successfully.")
+    else:
+        print("This username already exists.")
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,20 +1,48 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True)
-    password_hash = db.Column(db.String(128))
-    is_manager = db.Column(db.Boolean, default=False)
+class Employee(db.Model):
+    __tablename__ = 'employees'
+    emp_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    emp_name = db.Column(db.String(255), nullable=False)
+    scheduled_hrs = db.Column(db.Integer, nullable=True)
+    # Relationship to Attendance and LoginCredentials
+    attendances = db.relationship('Attendance', backref='employee', lazy=True)
+    login_credential = db.relationship('LoginCredentials', backref='employee', uselist=False, lazy=True)
 
-    # Relationship with Timesheet
-    timesheets = db.relationship('Timesheet', backref='user', lazy=True)
+class Department(db.Model):
+    __tablename__ = 'departments'
+    dept_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    dept_name = db.Column(db.String(255), nullable=False)
+    # Relationship to Attendance
+    attendances = db.relationship('Attendance', backref='department', lazy=True)
 
-class Timesheet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    clock_in_time = db.Column(db.DateTime, nullable=False)
-    clock_out_time = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+class Location(db.Model):
+    __tablename__ = 'locations'
+    location_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    location_name = db.Column(db.String(255), nullable=False)
+    # Relationship to Attendance
+    attendances = db.relationship('Attendance', backref='location', lazy=True)
 
-    # Add other fields as necessary
+class Attendance(db.Model):
+    __tablename__ = 'attendance'
+    entry_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(db.Date, nullable=False)
+    clock_in = db.Column(db.Time, nullable=True)
+    clock_out = db.Column(db.Time, nullable=True)
+    break_duration = db.Column(db.Time, nullable=True)
+    total_hrs = db.Column(db.Time, nullable=True)
+    status = db.Column(db.String(20), nullable=False)
+    emp_id = db.Column(db.Integer, db.ForeignKey('employees.emp_id'), nullable=True)
+    dept_id = db.Column(db.Integer, db.ForeignKey('departments.dept_id'), nullable=True)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'), nullable=True)
+
+class LoginCredentials(db.Model):
+    __tablename__ = 'login_credentials'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    is_manager = db.Column(db.Boolean, nullable=False, default=False)
+    emp_id = db.Column(db.Integer, db.ForeignKey('employees.emp_id'), nullable=False, unique=True)
