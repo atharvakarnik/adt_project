d4df6eaa068474c66cf939e24e15f435946bb4a3
Atharva Karnik <viroo.28@gmail.com>
2023-12-01 19:00:51 -0500

    Added constraints on data
    Added validation methods

--- backend/app/models.py
+++ backend/app/models.py
@@ -11,6 +11,19 @@ class Employee(db.Model):
     # Relationship to Attendance and LoginCredentials
     attendances = db.relationship('Attendance', backref='employee', lazy=True)
     login_credential = db.relationship('LoginCredentials', backref='employee', uselist=False, lazy=True)
+    
+    # Enforcing constraints
+    @validates('emp_name')
+    def validate_emp_name(self, key, emp_name):
+        if not emp_name:
+            raise ValueError("Employee name must not be empty")
+        return emp_name
+    
+    @validates('scheduled_hrs')
+    def validate_scheduled_hrs(self, key, scheduled_hrs):
+        if scheduled_hrs is not None and not (0 <= scheduled_hrs <= 24):
+            raise ValueError("scheduled_hrs must be between 0 and 24")
+        return scheduled_hrs
 
 class Department(db.Model):
     __tablename__ = 'departments'
@@ -19,12 +32,25 @@ class Department(db.Model):
     # Relationship to Attendance
     attendances = db.relationship('Attendance', backref='department', lazy=True)
 
+    # Enforcing constraints
+    def validate_dept_name(self, key, dept_name):
+        if not dept_name:
+            raise ValueError("Department name must not be empty")
+        return dept_name
+
 class Location(db.Model):
     __tablename__ = 'locations'
     location_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     location_name = db.Column(db.String(255), nullable=False)
     # Relationship to Attendance
     attendances = db.relationship('Attendance', backref='location', lazy=True)
+
+    # Enforcing constraints
+    @validates('location_name')
+    def validate_location_name(self, key, location_name):
+        if not location_name:
+            raise ValueError("Location name must not be empty")
+        return location_name
 
 class Attendance(db.Model):
     __tablename__ = 'attendance'
@@ -38,6 +64,21 @@ class Attendance(db.Model):
     emp_id = db.Column(db.Integer, db.ForeignKey('employees.emp_id'), nullable=True)
     dept_id = db.Column(db.Integer, db.ForeignKey('departments.dept_id'), nullable=True)
     location_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'), nullable=True)
+
+    # Enforcing constraints
+
+    @validates('clock_in', 'clock_out')
+    def validate_clock_times(self, key, time):
+        if key == 'clock_out' and self.clock_in and time:
+            if time < self.clock_in:
+                raise ValueError("Clock out time must be after clock in time")
+        return time
+    
+    @validates('status')
+    def validate_status(self, key, status):
+        if status not in ['Approved', 'Unapproved']:
+            raise ValueError("Status must be either 'Approved' or 'Unapproved'")
+        return status
 
 class LoginCredentials(db.Model):
     __tablename__ = 'login_credentials'
