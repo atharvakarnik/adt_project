f45152acf336730a30a02e47ce8505b6a8bacdd9
Atharva Karnik <viroo.28@gmail.com>
2023-12-02 22:27:34 -0500

    Changed data

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,11 +5,11 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/db.sqlite' 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'  
 
     db.init_app(app)
 
     with app.app_context():
-        db.create_all()
+        db.create_all()  
 
     return app
