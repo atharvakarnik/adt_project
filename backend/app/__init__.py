2656fd867d4816894b4c270c14f06ca7421f6850
Atharva Karnik <viroo.28@gmail.com>
2023-12-03 16:28:55 -0500

    Changed database route

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'  
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/db.sqlite'  
 
     db.init_app(app)
 
